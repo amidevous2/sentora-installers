debee635347e63807c9d7b8c98c42149fccca091
amidevous2 <amidevous+2@gmail.com>
2026-08-11 02:28:55 +0200

    Update ovh-soyoustar-kimsufi-postinstall.sh

--- ovh-soyoustar-kimsufi-postinstall.sh
+++ ovh-soyoustar-kimsufi-postinstall.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # postinstall script for ovh plateform ovh.com soyoustart.com and kimsufi.com
-wget http://sentora.org/install -O sentora_install.sh
-chmod +x sentora_install.sh
+if command -v curl >/dev/null 2>&1; then
+  curl -fL -o "/root/sentora_install.sh" "http://sentora.org/install"
+elif command -v wget >/dev/null 2>&1; then
+  wget "http://sentora.org/install" -O "/root/sentora_install.sh"
+fi
+chmod +x /root/sentora_install.sh
 if [ -f /etc/centos-release ]; then
 yum -y update
 yum -y remove bind
+yum clean all
 elif [ -f /etc/lsb-release ]; then
 apt-get update
 apt-get -y dist-upgrade
+apt-get clean
+apt-get autoclean
 fi
-./sentora_install.sh -t Europe/Paris -d $(hostname --fqdn) -i public
+/root/sentora_install.sh -t Europe/Paris -d $(hostname --fqdn) -i public
 echo "OK"
 exit
